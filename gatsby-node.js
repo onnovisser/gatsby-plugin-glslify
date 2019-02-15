f6eb1f3de4be474f516a7739be0d012a44baa7a8
Onno Visser <visser.onno@gmail.com>
2019-02-16 00:30:06 +0100

    Add more file types

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,9 @@
-exports.onCreateWebpackConfig = ({
-  loaders,
-  actions
-}) => {
+exports.onCreateWebpackConfig = ({ loaders, actions }) => {
   actions.setWebpackConfig({
     module: {
       rules: [
         {
-          test: /\.(glsl|frag|vert|comp)$/,
+          test: /\.(glsl|frag|vert|geom|comp|vs|fs|gs|vsh|fsh|gsh|vshader|fshader|gshader)$/,
           use: [loaders.raw(), "glslify-loader"]
         }
       ]
